279b2e363083572f66eef598e4b70c1c1e3d427d
PerkinArtem <68643457+PerkinArtem@users.noreply.github.com>
2020-08-22 11:36:00 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -109,43 +109,43 @@ function popupClose(popupActive) {
 
 
 
-// Отправка заявки 
-$(document).ready(function() {
-	$('#form').submit(function() { // проверка на пустоту заполненных полей. Атрибут html5 — required не подходит (не поддерживается Safari)
-		if (document.form.name.value == '' || document.form.email.value == '' || document.form.message.value == '') {
-			valid = false;
-			return valid;
-		}
-		$.ajax({
-			type: "POST",
-			url: "mail/mail.php",
-			data: $(this).serialize()
-		}).done(function() {
-            alert("Спасибо!");
-			// $('.js-overlay-thank-you').fadeIn();
-			// $(this).find('input').val('');
-			// $('#form').trigger('reset');
-		});
-		return false;
-	});
-});
-
-// // Закрыть попап «спасибо»
-// $('.js-close-thank-you').click(function() { // по клику на крестик
-// 	$('.js-overlay-thank-you').fadeOut();
+// // Отправка заявки 
+// $(document).ready(function() {
+// 	$('#form').submit(function() { // проверка на пустоту заполненных полей. Атрибут html5 — required не подходит (не поддерживается Safari)
+// 		if (document.form.name.value == '' || document.form.email.value == '' || document.form.message.value == '') {
+// 			valid = false;
+// 			return valid;
+// 		}
+// 		$.ajax({
+// 			type: "POST",
+// 			url: "mail/mail.php",
+// 			data: $(this).serialize()
+// 		}).done(function() {
+//             alert("Спасибо!");
+// 			// $('.js-overlay-thank-you').fadeIn();
+// 			// $(this).find('input').val('');
+// 			// $('#form').trigger('reset');
+// 		});
+// 		return false;
+// 	});
 // });
 
-// $(document).mouseup(function (e) { // по клику вне попапа
-// 	var popup = $('.popup');
-// 	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
-// 		$('.js-overlay-thank-you').fadeOut();
-// 	}
-// });
+// // // Закрыть попап «спасибо»
+// // $('.js-close-thank-you').click(function() { // по клику на крестик
+// // 	$('.js-overlay-thank-you').fadeOut();
+// // });
 
-// // Маска ввода номера телефона (плагин maskedinput)
-// $(function($){
-// 	$('[name="phone"]').mask("+7(999) 999-9999");
-// });
+// // $(document).mouseup(function (e) { // по клику вне попапа
+// // 	var popup = $('.popup');
+// // 	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+// // 		$('.js-overlay-thank-you').fadeOut();
+// // 	}
+// // });
+
+// // // Маска ввода номера телефона (плагин maskedinput)
+// // $(function($){
+// // 	$('[name="phone"]').mask("+7(999) 999-9999");
+// // });
 
 
 
--- mail.php
+++ mail.php
@@ -1,14 +1,14 @@
 <?php
 
-$recepient = "artem.perkin1999@gmail.com";
-$siteName = "Resume";
+// $recepient = "artem.perkin1999@gmail.com";
+// $siteName = "Resume";
 
-$name = trim($_POST["name"]);
-$email = trim($_POST["email"]);
-$text = trim($_POST["text"]);
-$message = "Имя: $name \nПочта: $email \nСообщение: $text";
+// $name = trim($_POST["name"]);
+// $email = trim($_POST["email"]);
+// $text = trim($_POST["text"]);
+// $message = "Имя: $name \nПочта: $email \nСообщение: $text";
 
-$pagetitle = "Заявка с сайта \"$siteName\"";
-mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+// $pagetitle = "Заявка с сайта \"$siteName\"";
+// mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
 
 ?>
