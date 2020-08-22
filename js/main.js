5c27a3830379326bc8769e00e7c5deed2b973e13
PerkinArtem <68643457+PerkinArtem@users.noreply.github.com>
2020-08-22 11:30:13 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -107,26 +107,45 @@ function popupClose(popupActive) {
 }
 
 
-$(document).ready(function() {
 
-	//E-mail Ajax Send
-	$("form").submit(function() { //Change
-		var th = $(this);
+
+// Отправка заявки 
+$(document).ready(function() {
+	$('#form').submit(function() { // проверка на пустоту заполненных полей. Атрибут html5 — required не подходит (не поддерживается Safari)
+		if (document.form.name.value == '' || document.form.email.value == '' || document.form.message.value == '') {
+			valid = false;
+			return valid;
+		}
 		$.ajax({
 			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
+			url: "mail/mail.php",
+			data: $(this).serialize()
 		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
+            alert("Спасибо!");
+			// $('.js-overlay-thank-you').fadeIn();
+			// $(this).find('input').val('');
+			// $('#form').trigger('reset');
 		});
 		return false;
 	});
-
 });
+
+// // Закрыть попап «спасибо»
+// $('.js-close-thank-you').click(function() { // по клику на крестик
+// 	$('.js-overlay-thank-you').fadeOut();
+// });
+
+// $(document).mouseup(function (e) { // по клику вне попапа
+// 	var popup = $('.popup');
+// 	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+// 		$('.js-overlay-thank-you').fadeOut();
+// 	}
+// });
+
+// // Маска ввода номера телефона (плагин maskedinput)
+// $(function($){
+// 	$('[name="phone"]').mask("+7(999) 999-9999");
+// });
 
 
 
